6390ab9a2a33ce2c6d19021fa839d43f171cd27b
isaacprogi <isaaconyes80@gmail.com>
2026-05-12 15:19:50 +0100

    chore: update next config

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,48 +1,33 @@
-// import { createMDX } from "fumadocs-mdx/next";
-
-// /** @type {import('next').NextConfig} */
-// const config = {
-//   reactStrictMode: true,
-
-//   // IMPORTANT: required for GitHub Pages
-//   output: "export",
-//   trailingSlash: true,
-
-//   // GitHub Pages base path (repo name)
-//   basePath: "/scaffoldrite",
-//   assetPrefix: "/scaffoldrite/",
-
-//   images: {
-//     // required for static export (no Next image optimization on GH Pages)
-//     unoptimized: true,
-
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "raw.githubusercontent.com",
-//       },
-//     ],
-//   },
-// };
-
-// const withMDX = createMDX({
-//   // optional:
-//   // configPath: "source.config.ts",
-// });
-
-// export default withMDX(config);
-
-
-import { createMDX } from 'fumadocs-mdx/next';
+import { createMDX } from "fumadocs-mdx/next";
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+
+  // IMPORTANT: required for GitHub Pages
+  output: "export",
+  trailingSlash: true,
+
+  // GitHub Pages base path (repo name)
+  basePath: "/scaffoldrite",
+  assetPrefix: "/scaffoldrite/",
+
+  images: {
+    // required for static export (no Next image optimization on GH Pages)
+    unoptimized: true,
+
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+      },
+    ],
+  },
 };
 
 const withMDX = createMDX({
-  // customize the config file path
-  // configPath: "source.config.ts"
+  // optional:
+  // configPath: "source.config.ts",
 });
 
 export default withMDX(config);
